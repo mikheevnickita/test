375f8f91892bb6976e0722509d58f4ab65070058
cjey <cjey@ya.ru>
2015-03-07 19:56:51 +0300

    Add PrivateFieldTest and DictionaryTest

---
+++ Test/DictionaryTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class DictionaryTest
+    {
+        private const Int32 N = 10000;
+        private const Int32 SimpleN = 10103;
+
+        public static void RunTest()
+        {
+
+            Double summ = 0;
+            var keys = new Int32[N];
+            var dict = new Dictionary<Int32, Double>(10000);
+
+            // filling dict
+            for (int i = 0; i < N; i++)
+            {
+                //keys[i] = i;
+                keys[i] = i*SimpleN;
+                dict.Add(keys[i], Math.Sqrt(i));
+            }
+
+            // calculation start
+            var start = DateTime.Now;
+            for (int i = 0; i < 10*N; i++)
+            {
+                summ += dict[keys[i%N]];
+            }
+            var end = DateTime.Now;
+
+            Console.WriteLine((end - start).TotalMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test/PrivateFieldTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace Test
+{
+    class PrivateFieldTest
+    {
+        class A
+        {
+            private int _a;
+
+            public A()
+            {
+                _a = 123;
+            }
+        }
+
+        class B : A
+        {
+            public Int32 Private
+            {
+                get
+                {
+                    return (Int32)(typeof(A).GetField("_a", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this));
+                }
+                set
+                {
+                    typeof(A).GetField("_a", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(this, value);
+                }
+            }
+        }
+
+        public static void RunTest()
+        {
+            B tmp = new B();
+
+            Console.WriteLine(tmp.Private);
+            tmp.Private = 321;
+            Console.WriteLine(tmp.Private);
+            
+            Console.ReadKey();
+        }
+    }
+}
